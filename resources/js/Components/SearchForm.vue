bf31849939c11f9b63c491f9c42d99dea7104fb0
juan pablo JPR <128744584+juanpablor20@users.noreply.github.com>
2024-10-21 20:14:50 -0500

    se añadio el buscador

--- resources/js/Components/SearchForm.vue
+++ resources/js/Components/SearchForm.vue
@@ -22,18 +22,30 @@
     },
     data() {
       return {
-        localSearch: this.search, // Inicializa una propiedad local con el valor de la prop
+        localSearch: this.search, // Inicializa con el valor de búsqueda
       };
     },
     watch: {
+      search(newValue) {
+        // Actualiza localSearch si la prop search cambia
+        this.localSearch = newValue;
+      },
       localSearch(newValue) {
-        this.$emit('update:search', newValue); // Emitimos el evento cuando cambie el valor
+        this.$emit('update:search', newValue);
+  
+        // Si el campo de búsqueda está vacío, realizar la búsqueda automáticamente
+        if (newValue.trim() === '') {
+          this.submitSearch();
+        }
       },
     },
     methods: {
       submitSearch() {
-        // Usamos la propiedad local para enviar la búsqueda
-        this.$inertia.get(this.route('equipments.index'), { search: this.localSearch });
+        // Si el campo está vacío, enviamos una búsqueda sin parámetros para restablecer los resultados
+        const searchQuery = this.localSearch.trim() === '' ? {} : { search: this.localSearch };
+  
+        // Realizamos la petición al servidor con el valor de búsqueda (o sin ella si está vacío)
+        this.$inertia.get(this.route('equipments.index'), searchQuery);
       },
     },
   };
